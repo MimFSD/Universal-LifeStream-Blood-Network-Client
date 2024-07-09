4738222d028a4091942a24dfafba98b5aa7b2dd4
MimFSD <mimislam878@gmail.com>
2024-07-10 01:03:04 +0600

    Request for donation

--- src/hooks/useAllDonaRequest.jsx
+++ src/hooks/useAllDonaRequest.jsx
@@ -3,7 +3,6 @@ import useAxiosSecure from './useAxiosSecure';
 
 const useAllDonaRequest = () => {
     const axiosSecure = useAxiosSecure();
-
     // tan stack query 
     const { refetch, data: allRequests=[]} = useQuery({
         queryKey: ['allRequests'],
@@ -11,9 +10,9 @@ const useAllDonaRequest = () => {
             const res = await axiosSecure.get('/all_donation_requests');
             return res.data;
         }
-        
+
     })
-    return [allRequests, refetch]
+         return [allRequests, refetch]
 };
 
 export default useAllDonaRequest;
