e80eba40314044f8e45e97210ee942f499e1a2a8
MimFSD <mimislam878@gmail.com>
2024-07-10 01:07:17 +0600

    All blogs deatils info

--- src/pages/BlogDetails/BlogDetails.jsx
+++ src/pages/BlogDetails/BlogDetails.jsx
@@ -16,9 +16,11 @@ const BlogDetails = () => {
                             {/* <div contentEditable='false' dangerouslySetInnerHTML={{ __html: 'hello' }}></div> */}
                             <h2 className=" md:w-[70%] lg:w-[60%] text-4xl font-bold mb-4">
                                 <div contentEditable='false' dangerouslySetInnerHTML={{ __html: title }}></div>
+                                
                             </h2>
                             <span className="bg-green-700 text-white p-2 rounded-lg text-sm">{status}</span>
                         </div>
+
                         <img className="w-full h-[380px] md:h-[580px] object-cover" src={photo} alt="" />
                         <div className="space-y-4 mt-8">
                             <p>
